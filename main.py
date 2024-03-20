a7704bf318468ab9b9b15e505d96fab0e4cf4068
Scoopsies <mooninites103@gmail.com>
2024-03-20 13:29:56 -0500

    Added a try except block to check for valid inputs

--- main.py
+++ main.py
@@ -8,18 +8,32 @@ def new_game():
         ['7','8','9']
     ]
     win = False
+    turn = 0
     
-    while win == False:
+    while win == False and turn < 9:
         print_board(board)
-        player_choice = int(input('Where do you want to play? \n \n')) - 1
-        y_axis = int((player_choice)/ 3)
-        x_axis = player_choice % 3
+
+        while True:
+            try:
+                player_choice = input('Where do you want to play? \n \n')
+                choice_index = int(player_choice) - 1
+                if 0 <= choice_index <= 8:
+                    break
+                else:
+                    raise ValueError()
+                    # print_board(board)
+                    # print(f'"{player_choice}" is not a valid input. Please choose a number between 1 and 9.')
+            except:
+                print_board(board)
+                print(f'{player_choice} is not a valid input. Please choose a number between 1 and 9.')
+
+        y_axis = int((choice_index)/ 3)
+        x_axis = choice_index % 3
         board[y_axis][x_axis] = 'X'
         win = win_checker(board)
+        turn += 1
 
     print_board(board)
     print('You Win!')
-
-    
 
 new_game()
